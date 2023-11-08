aadb43d643274e85597e7c5c35d77e51ace2776d
Sebastian Toulier <sebastian.toulier@encora.com>
2023-11-08 18:26:29 -0500

    asdaf555as

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,6 +28,8 @@ import TheWelcome from '../components/TheWelcome.vue'
   background-color: #fff;
   border-radius: 8px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  position: relative;
+  left: 55%;
 }
 
 .cover-title {
